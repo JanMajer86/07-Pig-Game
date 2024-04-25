ba654432fa30e4cb3ea94e822361203fd77a3799
JanMajer86 <majer.jan.1986@gmail.com>
2024-04-25 12:29:16 +0200

    player switching vip

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ const btnHold = document.querySelector('.btn--hold');
 
 // Variables
 
+// main score - player 0 : player 1
+const scores = [0, 0];
+
 let currentScore = 0;
+let activePlayer = 0;
+
 // Initializing Game
 const initGame = function () {
   score0El.textContent = 0;
@@ -29,16 +34,19 @@ const rollDice = function () {
   // 1. Generating a random dice roll
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
   console.log(diceNumber);
+
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${diceNumber}.png`;
+
   // 3. Check for rolled 1
   if (diceNumber !== 1) {
     currentScore += diceNumber;
-    current0El.textContent = currentScore; // CHANGE LATER!!!!
-  }
-  // Switch to next player
-  else {
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 };
 
